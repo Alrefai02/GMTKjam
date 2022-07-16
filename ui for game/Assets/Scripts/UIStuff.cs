d328e072d323952b3a3f70dea4ffee3b3183ef95
g6no <ga6aan@gmail.com>
2022-07-16 18:26:00 +0300

    ui thingy

--- ui for game/Assets/Scripts/UIStuff.cs
+++ ui for game/Assets/Scripts/UIStuff.cs
@@ -2,19 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public enum MenuState { START, OPTIONS, EXIT}
+public enum OptionsState {RES, FUL, VOL }
+
 public class UIStuff : MonoBehaviour
 {
     public GameObject rightArrow;
     public GameObject leftArrow;
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    public GameObject resolution;
+    public GameObject fullscreen;
+    public GameObject volume;
+
 
     public Text button;
+    public Text optionsText;
+    public Text title;
+
+    public Dropdown resDropdown;
+
+    public AudioMixer audioMixer;
+
+    public Resolution[] resolutions;
 
     public MenuState menuState;
+    public OptionsState optionsState;
 
     void Start()
     {
+        // creating resolution thingy, thanks brackeys
+        resolutions = Screen.resolutions;
+        
+        resDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int curResIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                curResIndex = i;
+        }
+        resDropdown.AddOptions(options);
+        resDropdown.value = curResIndex;
+        resDropdown.RefreshShownValue();
+
         menuState = MenuState.START;
         StartMenu();
     }
@@ -38,6 +77,17 @@ public class UIStuff : MonoBehaviour
             menuState = MenuState.EXIT;
             ExitMenu();
         }
+
+        if (optionsState == OptionsState.RES)
+        {
+            optionsState = OptionsState.FUL;
+            FulMenu();
+        }
+        else if (optionsState == OptionsState.FUL)
+        {
+            optionsState = OptionsState.VOL;
+            VolMenu();
+        }
     }
 
     public void PrevState()
@@ -51,6 +101,17 @@ public class UIStuff : MonoBehaviour
         {
             menuState = MenuState.OPTIONS;
             OptionsMenu();
+        }
+
+        if (optionsState == OptionsState.FUL)
+        {
+            optionsState = OptionsState.RES;
+            ResMenu();
+        }
+        else if (optionsState == OptionsState.VOL)
+        {
+            optionsState = OptionsState.FUL;
+            FulMenu();
         }
     }
 
@@ -66,5 +127,69 @@ public class UIStuff : MonoBehaviour
         button.text =  "Exit";
         rightArrow.SetActive(false);
         leftArrow.SetActive(true);
+    }
+
+    public void OpenOptions()
+    {
+        title.text = "Options";
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        optionsState = OptionsState.RES;
+        ResMenu();
+    }
+
+    void ResMenu()
+    {
+        optionsText.text = "Resolution";
+        fullscreen.SetActive(false);
+        volume.SetActive(false);
+        resolution.SetActive(true);
+        rightArrow.SetActive(true);
+        leftArrow.SetActive(false);
+    }
+
+    void FulMenu()
+    {
+        optionsText.text = "Fullscreen";
+        volume.SetActive(false);
+        resolution.SetActive(false);
+        fullscreen.SetActive(true);
+        rightArrow.SetActive(true);
+        leftArrow.SetActive(true);
+    }
+
+    void VolMenu()
+    {
+        optionsText.text = "Volume";
+        resolution.SetActive(false);
+        fullscreen.SetActive(false);
+        volume.SetActive(true);
+        rightArrow.SetActive(false);
+        leftArrow.SetActive(true);
+    }
+
+    public void buttonPress()
+    {
+        if (menuState == MenuState.OPTIONS)
+        {
+
+            OpenOptions();
+        }
+    }
+
+    public void SetVolume(float volumeSlider)
+    {
+        audioMixer.SetFloat("volume", volumeSlider);
+    }
+
+    public void setFullScreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution (int resIndex)
+    {
+        Resolution resolution = resolutions[resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
